53f5f22bbff5ada9f76a93926794b3975d6d1296
Kaung223 <kaungpyaeaung8123@gmail.com>
2025-09-04 10:48:09 +0630

    refactor: refactor the code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-export { en_to_jdn } from "./en_to_jdn";
-export { jdn_to_gregorian } from "./jdn_to_gregorian";
-export { thingyan_time } from "./thingyan_time";
-export { myanmar_compare } from "./myanmar_compare";
-export { en_to_mm } from "./en_to_mm";
-export { mm_to_en } from "./mm_to_en";
-export { mm_to_jln } from "./mm_to_jln";
-export { get_sabbath } from "./get_sabbath";
-export { tran_mm } from "./tran_mm";
-export { num_eng_to_mm } from "./num_eng_to_mm";
-export { num_mm_to_eng } from "./num_mm_to_eng";
+export { en_to_jdn } from "./mm-calendar/en_to_jdn";
+export { jdn_to_gregorian } from "./mm-calendar/jdn_to_gregorian";
+export { thingyan_time } from "./mm-calendar/thingyan_time";
+export { myanmar_compare } from "./mm-calendar/myanmar_compare";
+export { en_to_mm } from "./mm-calendar/en_to_mm";
+export { mm_to_en } from "./mm-calendar/mm_to_en";
+export { mm_to_jln } from "./mm-calendar/mm_to_jln";
+export { get_sabbath } from "./mm-calendar/get_sabbath";
+export { tran_mm } from "./mm-calendar/tran_mm";
+export { num_eng_to_mm } from "./mm-calendar/num_eng_to_mm";
+export { num_mm_to_eng } from "./mm-calendar/num_mm_to_eng";
 
 
